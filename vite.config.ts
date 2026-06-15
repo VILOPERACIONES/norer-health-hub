3042bf70fa8f171eea855d4de16f186f9545808b
Eliam Jesús Cauich Villanueva <eliamjesus213@gmail.com>
2026-06-15 12:59:31 -0600

    chore: add proxy to vite.config.ts for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,17 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
     hmr: {
       overlay: false,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   plugins: [
